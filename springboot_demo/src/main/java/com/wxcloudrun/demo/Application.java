8ba96483b52fd85bfdcc2eb06381b9e0eccc71ec
ocean.shen <ocean.shen@ipsos.com>
2021-10-27 15:34:37 +0800

    迭代

--- springboot_demo/src/main/java/com/wxcloudrun/demo/Application.java
+++ springboot_demo/src/main/java/com/wxcloudrun/demo/Application.java
@@ -2,8 +2,10 @@ package com.wxcloudrun.demo;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
 public class Application {
 
     public static void main(String[] args) {
